8b89972a78bfa191dc684f5a15a959c2532ff978
t-kyohei <kyohei.t.0602@gmail.com>
2020-04-12 12:35:19 +0900

    プッシュ通知テストパス修正

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener("push", function(event) {
         throw new Error('User not subscribed')
     })
     .then(function(res) {
-      return fetch('/notifications.json')
+      return fetch('notifications.json')
     })
     .then(function(res) {
       if (res.status === 200) {
