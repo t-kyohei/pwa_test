bd335bf7dfee50acba07f3c04db6165a1e6c5866
unknown <li3151@snet.isid.co.jp>
2020-04-14 12:18:44 +0900

    camera2

--- camera2/js/camera.js
+++ camera2/js/camera.js
@@ -22,3 +22,29 @@ function openCamera() {
             console.log(err);
         });
  }
+ 
+ video.addEventListener("click", function() {
+    take_picture();
+});
+
+//テキストクリック撮影（撮影トリガー 1）
+document.getElementById('rec').click(function() {
+    take_picture();
+});
+
+//撮影関数
+function take_picture() {
+    //videoのstreamをcanvasに書き出す方法
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    //videoの縦幅横幅を取得
+    var w = video.offsetWidth;
+    var h = video.offsetHeight;    
+    canvas.setAttribute("width", w);
+    canvas.setAttribute("height", h);
+    ctx.drawImage(video, 0, 0, w, h);
+
+    //canvasを更にimgに書き出す方法
+    var img = document.getElementById('img');
+    img.src = canvas.toDataURL('image/png');
+}
