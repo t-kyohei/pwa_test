9fe6705e52c273018a809ca5e795286b7996cd43
unknown <li3151@snet.isid.co.jp>
2020-04-15 17:20:10 +0900

    push7作成12

--- push7/firebase-messaging-sw.js
+++ push7/firebase-messaging-sw.js
@@ -18,3 +18,28 @@ messaging.setBackgroundMessageHandler(function (payload) {
   return self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
+
+
+
+var CACHE_NAME = 'pwa-push-test-caches';
+var urlsToCache = ['./index.html'];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
