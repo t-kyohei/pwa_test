8a706ef546a1fcf41e55c2300bc387e8dfdcebc0
unknown <li3151@snet.isid.co.jp>
2020-04-24 11:23:11 +0900

    位置情報バックグラウンド

--- js/payment.js
+++ js/payment.js
@@ -1,13 +1,44 @@
 var pay = function(){
 
 if(window.PaymentRequest) {
+
+    // google pay ÇÃê›íË
+    const googlePaymentDataRequest = {
+      environment: 'TEST',
+      apiVersion: 2,
+      apiVersionMinor: 0,
+      merchantInfo: {
+        // A merchant ID is available after approval by Google.
+        // @see {@link https://developers.google.com/pay/api/web/guides/test-and-deploy/integration-checklist}
+        // merchantId: '01234567890123456789',
+        merchantName: 'Example Merchant'
+      },
+      allowedPaymentMethods: [{
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          allowedCardNetworks: ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          // Check with your payment gateway on the parameters to pass.
+          // @see {@link https://developers.google.com/pay/api/web/reference/request-objects#gateway}
+          parameters: {
+            'gateway': 'example',
+            'gatewayMerchantId': 'exampleGatewayMerchantId'
+          }
+        }
+      }]
+    };
+    
+    
+
     //alert("Yes");
     // Yes, we can use the API
     
     const paymentMethods = [
-    {
-        supportedMethods: ['basic-card']
-    }
+    {supportedMethods: 'https://google.com/pay', data: googlePaymentDataRequest},
+    {supportedMethods: ['basic-card']}
 	];
 
 	const paymentDetails = {
