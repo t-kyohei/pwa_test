c7098c3159d3aa3fd429c4061f798b60542bf366
unknown <li3151@snet.isid.co.jp>
2020-04-24 15:59:33 +0900

    pay更新

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-var CACHE_NAME = 'pwa-camera-test-caches-009';
+var CACHE_NAME = 'pwa-camera-test-caches-010';
 var urlsToCache = ['./index.html?001', 
 					'./btn.css?001',
-					'./js/payment.js?003',
+					'./js/payment.js?004',
 					'./camera/',
 					'./location/index.html?001',
 					'./location/js/location.js',
