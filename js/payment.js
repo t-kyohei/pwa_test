ad421854509f19c7bac6da580504416a2e03b8d5
unknown <li3151@snet.isid.co.jp>
2020-05-01 12:57:40 +0900

    天気予報追加

--- js/payment.js
+++ js/payment.js
@@ -126,3 +126,77 @@ if(window.PaymentRequest) {
 
 }
 
+var pay2 = function(){
+
+if(window.PaymentRequest) {
+
+   
+    //alert("Yes");
+    // Yes, we can use the API
+    
+    const paymentMethods = [
+    {supportedMethods: ['basic-card'],
+    		data: {
+      			supportedNetworks: [
+        		'visa', 'mastercard', 'amex', 'diners', 'jcb'
+      			]
+    		}
+    }
+	];
+
+	const paymentDetails = {
+    total: {
+        label: 'Total Amount',
+        amount: {
+            currency: 'JPY',
+            value: 1
+        }
+    }
+	};
+	
+	
+
+  Object.assign(paymentDetails, { shippingOptions });
+	
+	const options = {
+        requestPayerName: true,
+      };
+
+	const paymentRequest = new PaymentRequest(
+    paymentMethods,
+    paymentDetails,
+    options
+	);
+    
+    
+    paymentRequest
+    .show()
+    .then(paymentResponse => {
+        // close the payment UI
+        paymentResponse.complete()
+            .then(() => {
+                // TODO: call REST API to process the payment at backend server
+                // with the data from `paymentResponse`.
+            });
+    })
+    .catch(err => {
+        // user closed the UI or the API threw an error
+        console.log('Error:', err);
+    });
+    
+    
+    
+    
+    
+} else {
+    // No, fallback to checkout page
+    alert("No");
+}
+
+
+
+
+}
+
+
+
